8a0a0f612312bbdaea13dc262dc13c303336c851
Michael Lin <mikelin@amazon.com>
2015-02-27 15:58:53 -0800

    set up basic dummy unit tests

--- src/GameOfLife/ConwayGameOfLife.java
+++ src/GameOfLife/ConwayGameOfLife.java
@@ -1,6 +1,10 @@
 public class ConwayGameOfLife {
-
+    
     public static void main(String[] args) {
         System.out.println("Hello World");
+    }
+
+    public int add(int x, int y) {
+        return x + y;
     }
 }
--- tst/ConwayGameOfLifeTests.java
+++ tst/ConwayGameOfLifeTests.java
@@ -1,3 +1,19 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ConwayGameOfLifeTests {
 
+    private ConwayGameOfLife mGame;
+
+    @Before
+    public void setUp() {
+        mGame = new ConwayGameOfLife();
+    }
+    
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(5, mGame.add(3, 2));
+    }
 }
